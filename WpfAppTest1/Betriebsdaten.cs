1f4922bb03ba31c8e06f1f224134a30af76bc12a
Alkaensos <90407380+Alkaensos@users.noreply.github.com>
2023-05-21 23:12:27 +0200

    Zustandssimulation

--- WpfAppTest1/Betriebsdaten.cs
+++ WpfAppTest1/Betriebsdaten.cs
@@ -1,12 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppTest1
 {
-    internal class Betriebsdaten
+    public class Betriebsdaten
     {
+        private int dauer;
+        public int Dauer
+        {
+            get { return dauer; }
+            set { dauer = value; }
+        }
+        
+        private int drehzahl;
+
+        public int Drehzahl
+        {
+            get { return drehzahl; }
+            set { drehzahl = value; }
+        }
+
+        private int leistungsaufnahme;
+
+        public int Leistungsaufnahme
+        {
+            get { return leistungsaufnahme; }
+            set { leistungsaufnahme = value; }
+        }
+
+        private int strom;
+
+        public int Strom
+        {
+            get { return strom; }
+            set { strom = value; }
+        }
+
+        private int spannung;
+
+        public int Spannung
+        {
+            get { return spannung; }
+            set { spannung = value; }
+        }
+
+        public void getStufe0()
+        {
+            Dauer = 5;
+            Drehzahl = 0;
+            Leistungsaufnahme = 0;
+            Strom = 0;
+            Spannung = 0;
+        }
+
+        public void getStufe1()
+        {
+            Dauer = 5;
+            Drehzahl = 30;
+            Leistungsaufnahme = 15;
+            Strom = Leistungsaufnahme / Spannung;
+            Spannung = 230;
+        }
+
+        public void getStufe2()
+        {
+            Dauer = 5;
+            Drehzahl = 60;
+            Leistungsaufnahme = 45;
+            Strom = Leistungsaufnahme / Spannung;
+            Spannung = 230;
+        }
+
+        public void getStufe3()
+        {
+            Dauer = 5;
+            Drehzahl = 90;
+            Leistungsaufnahme = 60;
+            Strom = Leistungsaufnahme / Spannung;
+            Spannung = 230;
+        }
     }
 }
--- WpfAppTest1/Zustandssimulation.cs
+++ WpfAppTest1/Zustandssimulation.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace WpfAppTest1
 {
-    internal class Zustandssimulation
+    public class Zustandssimulation
     {
+        private int aktuelleDrehzahl;
+
+        public int AktuelleDrehzahl
+        {
+            get { return aktuelleDrehzahl; }
+            set { aktuelleDrehzahl = value; }
+        }
+
+        private int AktuelleLeistungsaufnahme;
+
+        public int aktuelleLeistungsaufnahme
+        {
+            get { return aktuelleLeistungsaufnahme; }
+            set { aktuelleLeistungsaufnahme =  value; }
+        }
+
+        private int AktuellerStrom;
+
+        public int aktuellerStrom
+        {
+            get { return aktuellerStrom; }
+            set { aktuellerStrom = value; }
+        }
+
+        private int AktuelleSpannung;
+
+        public int aktuelleSpannung
+        {
+            get { return aktuelleSpannung; }
+            set { aktuelleSpannung  = value; }
+        }
+
+        private int SollDrehzahl;
+
+        public int sollDrehzahl
+        {
+            get { return sollDrehzahl; }
+            set {  sollDrehzahl = value;}
+        }
+        private int SollLeistungsaufnahme;
+
+        public int sollLeistungsaufnahme
+        {
+            get { return sollLeistungsaufnahme; }
+            set { sollLeistungsaufnahme = value; }
+        }
+
+        private int SollStrom;
+
+        public int sollStrom
+        {
+            get { return sollStrom; }
+            set { sollStrom = value; }
+        }
+
+        private int SollSpannung;
+
+        public int sollSpannung
+        {
+            get { return sollSpannung; }
+            set { sollSpannung = value;}
+        }
+
+        private int SollDauer;
+
+        public int sollDauer
+        {
+            get { return sollDauer; }
+            set { sollDauer = value;}
+        }
     }
 }
