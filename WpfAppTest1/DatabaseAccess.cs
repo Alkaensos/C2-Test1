6584e82ce2e9d09764f72b89ef7bf4d461d608f5
Alkaensos <90407380+Alkaensos@users.noreply.github.com>
2023-05-21 16:55:51 +0200

    Database Access

---
+++ WpfAppTest1/DatabaseAccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTest1
+{
+    public class DatabaseAccess
+    {
+        public static int executeQuery(string sql, params object[] args)
+        {
+            int rowCount = 0;
+            SqlConnection lueftungssteuerungDb =
+                new SqlConnection(System.Configuration.ConfigurationSettings.AppSettings["Lueftungssteuerung"]);
+            SqlCommand sqlCommand = new SqlCommand();
+
+            try
+            {
+                sqlCommand.CommandText = sql;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Connection = lueftungssteuerungDb;
+                sqlCommand.CommandTimeout = 12 * 3600;
+
+                lueftungssteuerungDb.Open();
+
+                rowCount = sqlCommand.ExecuteNonQuery();
+                lueftungssteuerungDb.Close();
+            }
+            catch (Exception ex)
+            {
+                lueftungssteuerungDb.Close();
+                throw ex;
+            }
+            return rowCount;
+        }
+    }
+}
